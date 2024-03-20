8a0fc774bd4565c1ba9de84473a476eeb4075522
Алексей <iumat0v@yandex.ru>
2024-03-20 19:18:47 +0300

    основа

--- main.py
+++ main.py
@@ -5,7 +5,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return render_template('base.html')
+    return render_template('main.html')
+
+
+@app.route('/soups')
+def soups():
+    return 'soups'
+
+
+@app.route('/drinks')
+def drinks():
+    return 'Drinks'
+
+
+@app.route('/desserts')
+def desserts():
+    return 'desserts'
+
+
+@app.route('/salads')
+def salads():
+    return 'salads'
+
+
+@app.route('/second_dish')
+def second_dish():
+    return 'second dish'
 
 
 if __name__ == '__main__':
