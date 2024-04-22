4e1f0671ca77e73a23af35c03c148721c90293ea
Алексей <iumat0v@yandex.ru>
2024-04-22 19:42:14 +0300

    -

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user
 
 from data import db_session
 from data.recipes import Recipe
@@ -9,6 +9,8 @@ from data.images import Image
 from data.rec_images import RecImage
 from data.users import User
 from forms.user import RegisterForm
+from data.users import User
+from forms.user import RegisterForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -51,6 +53,20 @@ def reqister():
         db_sess.commit()
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
+
+'''@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)'''
 
 
 @app.route('/soups')
