109d2b4c8b6507c297b08124869408dc63135fb0
Алексей <iumat0v@yandex.ru>
2024-04-22 18:44:26 +0300

    регистрация, авторизация - не работает

--- main.py
+++ main.py
@@ -18,76 +18,33 @@ def main():
 @app.route('/soups')
 def soups():
     db_sess = db_session.create_session()
-    temp = [el.id for el in db_sess.query(RecCat).filter(RecCat.id_cats == 1).all()]
+    temp = [el.id for el in db_sess.query(RecCat).filter(RecCat.id_cats==1).all()]
     soups = db_sess.query(Recipe).filter(Recipe.id.in_(temp)).all()
     return render_template('list_soups.html', soups=soups)
-
 
 @app.route('/soup/<int:pk>')
 def soup(pk):
     db_sess = db_session.create_session()
     soup = db_sess.query(Recipe).filter(Recipe.id==pk).first()
     return render_template('detail_soup.html', soup=soup)
-
-
 @app.route('/drinks')
 def drinks():
-    db_sess = db_session.create_session()
-    temp = [el.id for el in db_sess.query(RecCat).filter(RecCat.id_cats == 3).all()]
-    drinks = db_sess.query(Recipe).filter(Recipe.id.in_(temp)).all()
-    return render_template('list_drinks.html', drinks=drinks)
-
-
-@app.route('/drink/<int:pk>')
-def drink(pk):
-    db_sess = db_session.create_session()
-    drink = db_sess.query(Recipe).filter(Recipe.id==pk).first()
-    return render_template('detail_drink.html', drink=drink)
+    return 'Drinks'
 
 
 @app.route('/desserts')
 def desserts():
-    db_sess = db_session.create_session()
-    temp = [el.id for el in db_sess.query(RecCat).filter(RecCat.id_cats == 4).all()]
-    desserts = db_sess.query(Recipe).filter(Recipe.id.in_(temp)).all()
-    return render_template('list_desserts.html', desserts=desserts)
-
-
-@app.route('/dessert/<int:pk>')
-def dessert(pk):
-    db_sess = db_session.create_session()
-    dessert = db_sess.query(Recipe).filter(Recipe.id==pk).first()
-    return render_template('detail_dessert.html', dessert=dessert)
+    return 'desserts'
 
 
 @app.route('/salads')
 def salads():
-    db_sess = db_session.create_session()
-    temp = [el.id for el in db_sess.query(RecCat).filter(RecCat.id_cats == 5).all()]
-    salads = db_sess.query(Recipe).filter(Recipe.id.in_(temp)).all()
-    return render_template('list_salads.html', salads=salads)
+    return 'salads'
 
 
-@app.route('/salad/<int:pk>')
-def salad(pk):
-    db_sess = db_session.create_session()
-    salad = db_sess.query(Recipe).filter(Recipe.id==pk).first()
-    return render_template('detail_salad.html', salad=salad)
-
-
-@app.route('/second_dishes')
-def second_dishes():
-    db_sess = db_session.create_session()
-    temp = [el.id for el in db_sess.query(RecCat).filter(RecCat.id_cats == 2).all()]
-    second_dishes = db_sess.query(Recipe).filter(Recipe.id.in_(temp)).all()
-    return render_template('list_second_dishes.html', second_dishes=second_dishes)
-
-
-@app.route('/second_dish/<int:pk>')
-def second_dish(pk):
-    db_sess = db_session.create_session()
-    second_dish = db_sess.query(Recipe).filter(Recipe.id==pk).first()
-    return render_template('detail_second_dish.html', second_dish=second_dish)
+@app.route('/second_dish')
+def second_dish():
+    return 'second dish'
 
 
 if __name__ == '__main__':
